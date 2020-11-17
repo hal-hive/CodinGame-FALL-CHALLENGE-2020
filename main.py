b1e64eb66029cd8b838d00d0f086e1ac24e1767e
hkravchuk <hkravchuk@scalhive.com>
2020-11-17 19:03:50 +0200

    rewritten the first league

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 import numpy as np
 
 
+answer = 0
+
 def print_h():
     recipes = np.zeros(shape=(5, 11))
     spells = np.zeros(shape=(4, 6))
@@ -13,7 +15,7 @@ def print_h():
     recipes[0, 0:6] = [70, -2, -2, 0, -2, 15]
     recipes[1, 0:6] = [71, -2, 0, -3, 0, 11]
     recipes[2, 0:6] = [72, 0, -2, -3, 0, 13]
-    recipes[3, 0:6] = [73, -2, 0, 0, -2, 10]
+    recipes[3, 0:6] = [73, -2, 0, 0, 0, 10]
     recipes[4, 0:6] = [74, -2, 1, 0, 1, 9]
 
     spells[0, :] = [80, 2, 0, 0, 0, 1]
@@ -31,27 +33,37 @@ def print_h():
 
     l: int = 0
     while l < 5:
+        m = []
         for i in recipes[:, 6:10]:
             for j in i:
                 if j < 0:
-                    recipes[l, 10] = 0
-                    l = l + 1
-                    break
+                    m.append(False)
                 else:
-                    recipes[l, 10] = 1
-                    l = l + 1
+                    m.append(True)
+            a = all(m)
+            if a == False:
+                recipes[l, 10] = 0
+                l = l + 1
+                m.clear()
+            else:
+                recipes[l, 10] = 1
+                l = l + 1
+                m.clear()
 
-            # a = all(m)
-            # if a == False:
-            #    recipes[l, 10] = 0
-            #    l = l + 1
-            #    m.clear()
-            # else:
-            #    recipes[l, 10] = 1
-            #    l = l + 1
-            #    m.clear()
+    x = np.nonzero(recipes[:, 10])
+    if np.count_nonzero(x) == 1:
+        answer = np.take(recipes[:, 0], x)
+    elif np.count_nonzero(x) > 1:
+        ind_price = np.take(recipes[:, 5], x)
+        pric = ind_price.max()
+        ans = recipes[recipes[:, 5] == pric]
+        answer = ans[0,0]
+    else:
 
-    print(recipes)
+
+
+
+    print(answer)
 
 
 # Press the green button in the gutter to run the script.
